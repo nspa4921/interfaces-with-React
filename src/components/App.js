e95fa6bf7f30e87c24206761a3d10a4b92ea9e2c
Nemanja Spasic <nemo.spaske@live.com>
2020-11-12 12:19:37 +0100

    Sorting existing records

---
+++ src/components/App.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import '../css/App.css';
+
+import AddAppointments from './AddAppointments';
+import SearchAppointments from './SearchAppointments';
+import ListAppointments from './ListAppointments';
+
+import { without } from 'lodash';
+
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      myAppointments: [],
+      formDisplay: false,
+      orderBy: 'petName',
+      orderDir: 'desct',
+      lastIndex: 0
+    };
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.addAppointment = this.addAppointment.bind(this);
+    this.changeOrder = this.changeOrder.bind(this);
+
+
+
+  }
+
+  toggleForm() {
+    this.setState({
+      formDisplay: !this.state.formDisplay
+    });
+  }
+
+  changeOrder(order, dir) {
+    this.setState({
+      orderBy: order,
+      orderDir: dir
+    })
+  }
+
+  addAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    apt.aptId = this.state.lastIndex;
+    tempApts.unshift(apt);
+    this.setState({
+      myAppointments: tempApts,
+      lastIndex: this.state.lastIndex + 1
+    });
+  }
+
+  deleteAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    tempApts = without(tempApts, apt);
+
+    this.setState({
+      myAppointments: tempApts,
+    })
+  }
+
+  componentDidMount() {
+    fetch('./data.json')
+    .then(response => response.json())
+    .then(result => {
+      const apts = result.map(item => {
+        item.aptId = this.state.lastIndex;
+        this.setState({ lastIndex: this.state.lastIndex + 1})
+        return item;
+      });
+      this.setState({
+        myAppointments: apts,
+      });
+    })
+  }
+  
+  render() {
+    
+    let order; 
+    let filteredApts = this.state.myAppointments;
+    if(this.state.orderDir === 'asc') {
+      order = 1;
+    } else {
+      order = -1;
+    }
+
+    filteredApts.sort((a,b) => {
+      if(a[this.state.orderBy].toLowerCase() <
+         b[this.state.orderBy].toLowerCase()
+      ) {
+        return -1 * order;
+      }else {
+        return 1 * order;
+      }
+    })
+
+
+    return (
+      <main className="page bg-white" id="petratings">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 bg-white">
+            <div className="container">
+              <AddAppointments 
+              formDisplay={this.state.formDisplay}
+              toggleForm={this.toggleForm}
+              addAppointment={this.addAppointment}/>
+              <SearchAppointments
+              orderBy = {this.state.orderBy}
+              orderDir ={this.state.orderDir}
+              changeOrder={this.changeOrder}/>
+              <ListAppointments 
+              appointments={filteredApts}
+              deleteAppointment={this.deleteAppointment}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    );
+  }
+}
+
+export default App;
